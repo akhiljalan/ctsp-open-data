6147bd8b923713dbef5ef8976946b24e0d3986c7
Akhil Jalan <akhiljalan@berkeley.edu>
2018-04-15 12:00:46 -0700

    write code for glove embeddings

--- CTSP/code/load_categories.py
+++ CTSP/code/load_categories.py
@@ -3,8 +3,14 @@ def load_unique(filename):
 		all_cat = set(f.readline().split(','))
 	return all_cat
 
-def main(): 
-	print(load_unique('../categories.txt'))
+def load_with_repeats(filename): 
+	with open(filename, 'r') as f:
+		all_cat = f.readline().split(',')
+	return all_cat
+
+
+def get_categories_from_txt():
+	return load_unique('../categories.txt')
 
 if __name__ == '__main__':
-	main()
+	print('No main lol')
