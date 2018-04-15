86341a15d9a1748c6a2ba452a8f928d806153f8f
Ameet Rahane <ameetrahane@berkeley.edu>
2018-04-15 01:14:41 -0700

    added data num functions

--- CTSP/code/data_amount.py
+++ CTSP/code/data_amount.py
@@ -1,9 +1,23 @@
-import networkx as nx
 from Utils import *
+import re
+from urllib import request
 
 
 def main():
-	return ...
+	return get_string_regex('https://data.sfgov.org/browse')
+
+def get_num_datasets(data_cat_url):
+	html = request.urlopen(data_cat_url).read()
+	regexed_str = re.compile(r'<div class="browse2-results-title">(.+)</div>', re.DOTALL).search(html).group(0)
+	temp = regexed_str.split('div')
+	for i in temp[1].split():
+		try:
+			temp2 = int(i)
+		except:
+			pass
+	return temp2
 
 if __name__ == '__main__':
-	main()
+	data_set_name = main()
+	write_object(data_set_name, '../data/num_of_data_sets')
+	print(read_object('../data/num_of_data_sets'))
