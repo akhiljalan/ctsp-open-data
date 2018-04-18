2b21b640099432c752fc1a46fa26d916db46b108
Ameet Rahane <ameetrahane@berkeley.edu>
2018-04-18 13:22:10 -0700

    added recency functions

--- CTSP/code/recency.py
+++ CTSP/code/recency.py
@@ -1,9 +1,12 @@
 import networkx as nx
 from Utils import *
+from urllib import request
+import re
+from sodapy import Socrata
 
-
-def main():
+def main(url = 'https://data.cityofberkeley.info/311/311-Cases/k489-uv4i'):
 	return ...
 
 if __name__ == '__main__':
 	main()
+
