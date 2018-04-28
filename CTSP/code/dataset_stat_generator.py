2d52313867a726f04b36b22c346b744344831649
Ameet Rahane <ameetrahane@berkeley.edu>
2018-04-28 16:01:02 -0700

    added data

--- CTSP/code/dataset_stat_generator.py
+++ CTSP/code/dataset_stat_generator.py
@@ -10,14 +10,14 @@ import json
 import pandas as pd
 
 def get_stats(url, data_url):
-    """
+	"""
     Takes open data platform url and dataset url and gets significant statistics from it, 
     writes to csv. 
     """
-    try:
+	try:
 		json_url = "{}/api/views/{}/rows.json?accessType=DOWNLOAD".format(url, data_url[-9:])
 		with request.urlopen(json_url) as jurl:
-		    data = json.loads(jurl.read().decode())
+			data = json.loads(jurl.read().decode())
 		name =  data['meta']['view']['name']
 		created = data['meta']['view']['createdAt']
 		category = data['meta']['view']['category']
@@ -28,8 +28,8 @@ def get_stats(url, data_url):
 		tags = breadth.get_key_words(data_url)
 	except:
 		return None
-    return {'Name': [name],'URL': [data_url], 'Category': [category], 'Created': [created], 
-    'Index Updated': [indexUpdated], 'Number of Downloads': [downloadCount], 'Number of Views': [viewCount], 'View Last Modified': [viewLastModified], 'Tags': [tags]}
+	return {'Name': [name],'URL': [data_url], 'Category': [category], 'Created': [created], 
+	'Index Updated': [indexUpdated], 'Number of Downloads': [downloadCount], 'Number of Views': [viewCount], 'View Last Modified': [viewLastModified], 'Tags': [tags]}
 
 
 def write_all_csv(filename, data_dict):
